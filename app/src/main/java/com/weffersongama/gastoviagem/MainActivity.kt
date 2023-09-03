9f5ea08b2446b84d58815b9e5bf9c20bab27bba8
Wefferson <weffersongama@hotmail.com>
2023-09-02 22:53:18 -0300

    ViewBinding configuration, implementation of "onClick" and "calculate" functions.

--- app/src/main/java/com/weffersongama/gastoviagem/MainActivity.kt
+++ app/src/main/java/com/weffersongama/gastoviagem/MainActivity.kt
@@ -2,10 +2,30 @@ package com.weffersongama.gastoviagem
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.weffersongama.gastoviagem.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonCalculate.setOnClickListener(this)
+    }
+
+    override fun onClick(view: View) {
+        if (view.id == R.id.button_calculate) calculate()
+    }
+
+    private fun calculate() {
+        val distance = binding.editDistance.text.toString().toFloat()
+        val price = binding.editPrice.text.toString().toFloat()
+        val autonomy = binding.editAutonomy.text.toString().toFloat()
+
+        val totalValue = (distance * price) / autonomy
+
+        binding.textAmount.text = "R$ ${"%.2f".format(totalValue)}"
     }
 }
